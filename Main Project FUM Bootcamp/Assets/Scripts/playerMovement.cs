96a9da9a2613f7986030e5ee43caa065269976fb
ElyasTH <elyastashakkori@gmail.com>
2022-07-09 20:00:52 +0430

    added plate acceleration

--- Main Project FUM Bootcamp/Assets/Scripts/playerMovement.cs
+++ Main Project FUM Bootcamp/Assets/Scripts/playerMovement.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public int speed = 4000;
+    public int speed = 15;
     public Transform player;
+    public float max_acceleration = 1;
+
+    public float current_acceleration = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +15,21 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("a") && player.position.x > -5.3){
-            player.position = new Vector3(player.position.x - speed*Time.deltaTime, player.position.y, player.position.z);
+        if (Input.GetKey("a") && current_acceleration >= -max_acceleration){
+            current_acceleration -= 0.05f;
+        }
+        
+        if (Input.GetKey("d") && current_acceleration <= max_acceleration){
+            current_acceleration += 0.05f;
         }
 
-        if (Input.GetKey("d") && player.position.x < 7.9){
-            player.position = new Vector3(player.position.x + speed*Time.deltaTime, player.position.y, player.position.z);
+        else if (current_acceleration > 0) current_acceleration -= 0.05f;
+        else if (current_acceleration < 0 && !Input.GetKey("a")) current_acceleration += 0.05f;
+        if (current_acceleration > -0.05 && current_acceleration < 0.05) current_acceleration = 0;
+
+        if ((current_acceleration < 0 && player.position.x < -5.2) || (current_acceleration > 0 && player.position.x > 7.8)){
+            current_acceleration = 0;
         }
+        player.position = new Vector3(player.position.x + current_acceleration*speed*Time.deltaTime, player.position.y, player.position.z);
     }
 }
